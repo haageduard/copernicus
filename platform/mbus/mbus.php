89089f26f7873a4e6ad8b763896258f3c1fb07fa
haageduard <haageduard@gmail.com>
2015-06-04 18:20:49 +0700

    Create mbus.php

---
+++ platform/mbus/mbus.php
@@ -0,0 +1,18 @@
+<?php
+
+   /**
+   * MBUS - мастер шина данных
+   *
+   *   Поддерживаемые ОС:
+   *
+   *     Windows
+   *     Linux
+   *
+   * @package default
+   * @author cj
+   * @version 1
+   * @path platform/mbus/
+   *
+   **/
+   
+?>
