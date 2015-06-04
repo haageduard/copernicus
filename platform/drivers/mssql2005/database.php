df9fe4720892c0b8cd04cf76d4aad1b339296327
haageduard <haageduard@gmail.com>
2015-06-04 18:01:34 +0700

    Create database.php

---
+++ platform/drivers/mssql2005/database.php
@@ -0,0 +1,28 @@
+<?php 
+
+  /**
+   * Драйвер для работы с базой данных MS SQL 2005
+   *
+   *   Поддерживаемые ОС:
+   *
+   *     Windows
+   *     Linux-based (sybase)
+   *
+   * @package default
+   * @author cj
+   * @version 1
+   * @path platform/drivers/database/mssql2005/
+   *
+   **/
+
+  class DatabaseDriver implements IDatabaseDriver {
+
+    public function __construct() {
+    }
+
+    public function execQuery() {
+    }
+
+  }
+
+?>
