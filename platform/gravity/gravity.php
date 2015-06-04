5ee113e20fa62f7513d30e8f4482f5d8eaddde43
haageduard <haageduard@gmail.com>
2015-06-04 18:17:13 +0700

    Create gravity.php

---
+++ platform/gravity/gravity.php
@@ -0,0 +1,28 @@
+<?php
+
+   /**
+   * Подсистема Gravity для передачи данных по поддерживаемым транспортным протоколам
+   * 
+   *
+   *   Поддерживаемые ОС:
+   *
+   *     Windows
+   *     Linux
+   *
+   *   Поддерживаемые протколы
+   *
+   *     WebSockets
+   *     TCP sockets
+   *     Samba
+   *     SMTP
+   *     IMAP
+   *     POP3
+   *
+   * @package default
+   * @author cj
+   * @version 1
+   * @path platform/gravity/
+   *
+   **/
+   
+?>
