9a81f4a6938945ed58c32ed65ec873777878af4d
haageduard <haageduard@gmail.com>
2015-07-06 11:32:41 +0700

    Create ktheme.js

---
+++ platform/klib/ktheme.js
@@ -0,0 +1,13 @@
+function KTheme() {
+
+  var _author = '';
+  
+}
+
+KTheme.prototype.getAuthor = function() {
+  return _author;
+}
+
+KTheme.prototype.setAuthor = function(v) {
+  this._author = v;
+}
