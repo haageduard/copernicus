d1448d11e567cf3c50516208f9b3fdfc4d78b8d9
haageduard <haageduard@gmail.com>
2015-07-06 12:16:40 +0700

    Update app.js

--- apps/demo/app.js
+++ apps/demo/app.js
@@ -1,5 +1,11 @@
 function onStart() {
+  var demoView = findViewById('l');
+  var tv1 = new KTextView();
+  tv1.setText('Hello, world!');
+  tv1.setPaddingLeft(5);
+  tv1.setPaddingRight(5);
+  demoView.addView(tv1);
 }
 
-function onStop() {
+function onStop(reason) {
 }
