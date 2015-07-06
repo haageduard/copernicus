118e82ac8a9eb5db8c8366eacf84eb884c99fe6b
haageduard <haageduard@gmail.com>
2015-07-06 12:44:57 +0700

    Update p.php

--- platform/plugins/export_to_xls/p.php
+++ platform/plugins/export_to_xls/p.php
@@ -1,12 +1,19 @@
 <?php
 
-  class ExportToXLS implements ExportDataPlugin {
+  namespace Plugins;
+
+  class ExportToExcel implements Plugins.ExportTablePlugin {
   
     public function ExportToXLS($pluginManager, $modelId) {
     }
     
     public function export() {
       $tmp_dir = $pluginManager->getTemporaryDir();
+      $data = $pluginManager->getModel($modelId);
+    }
+    
+    public function onDestroy() {
+      $tmp_dir->purge();
     }
     
   }
